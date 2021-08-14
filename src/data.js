ecaecba248f4c6fd737edba2e011a3141438bc2c
RishabK05 <86464422+RishabK05@users.noreply.github.com>
2021-08-14 13:02:33 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,88 @@
+import img1 from "./images/img1.png"
+import img2 from './images/img2.png'
+import img3 from './images/img3.png'
+import img4 from './images/img4.png'
+import img5 from './images/img5.png'
+import img6 from './images/img6.png'
+import img7 from './images/img7.png'
+import img8 from './images/img8.png'
+
+const data ={
+    cardData:[
+        {
+            id: 1,
+            img : img1,
+            name: "Leanne Graham",
+            username: "Bret",
+            email: "Sincere@april.biz"
+
+        },
+        {
+            id: 2,
+            img : img2,
+            name: "Ervin Howell",
+            username: "Antonette",
+            email: "Shanna@melissa.tv"
+        },
+        {
+            id: 3,
+            img : img3,
+            name: "Clementine Bauch",
+            username: "Samantha",
+            email: "Nathan@yesenia.net"
+        },
+        {
+            id: 4,
+            img : img4,
+            name: "Patricia Lebsack",
+            username: "Karianne",
+            email: "Julianne.OConner@kory.org"
+        },
+        {
+            id: 5,
+            img : img5,
+            name: "Chelsey Dietrich",
+            username: "Kamren",
+            email: "Lucio_Hettinger@annie.ca"
+
+        },
+        {
+            id: 6,
+            img : img6,
+            name: "Mrs. Dennis Schulist",
+            username: "Leopoldo_Corkery",
+            email: "Karley_Dach@jasper.info"
+
+        },
+        {
+            id: 7,
+            img : img7,   
+            name: "Kurtis Weissnat",
+            username: "Elwyn.Skiles",
+            email: "Telly.Hoeger@billy.biz"
+        },
+        {
+            id: 8,
+            img : img8,
+            name: "Nicholas Runolfsdottir V",
+            username: "Maxime_Nienow",
+            email: "Sherwood@rosamond.me"
+        },
+        {
+            id: 9,
+            img: img1,
+            name: "Glenna Reichert",
+            username: "Delphine",
+            email: "Chaim_McDermott@dana.io"
+        },
+        {
+            id: 10,
+            img: img2,
+            name: "Clementina DuBuque",
+            username: "Moriah.Stanton",
+            email: "Rey.Padberg@karina.biz"
+        }
+    ]
+}
+
+export default data;
